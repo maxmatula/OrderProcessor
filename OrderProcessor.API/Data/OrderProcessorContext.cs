ce2e00963ffca244b836ec98ae99f102ba48665e
Maksymilian <maksymilian.matula@gmail.com>
2018-11-18 17:47:07 +0100

    InitialCommit

---
+++ OrderProcessor.API/Data/OrderProcessorContext.cs
@@ -0,0 +1,9 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace OrderProcessor.API.Data
+{
+    public class OrderProcessorContext : DbContext
+    {
+        // public DbSet<> xxx { get; set; }
+    }
+}
