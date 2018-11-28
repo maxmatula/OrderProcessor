64acc8757f7dcda0d2ee0022f6c44d1cd84cdbfd
Maksymilian <maksymilian.matula@gmail.com>
2018-11-28 20:46:05 +0100

    fixed cart model

--- OrderProcessor.API/Models/Cart.cs
+++ OrderProcessor.API/Models/Cart.cs
@@ -5,6 +5,8 @@ namespace OrderProcessor.API.Models
     public class Cart
     {
         public int UserId;
+        public int AddressId;
+        public int CurrencyId;
         public List<CartLine> Lines { get; set; }
     }
 }
